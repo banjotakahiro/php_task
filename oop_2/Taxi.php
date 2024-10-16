6e1ee1df0a5bcd4e1901dffb23d43c4c77e4096e
banjotakahiro <watawata20220404@gmail.com>
2024-10-16 16:54:52 +0900

    修正した課題

--- oop_2/Taxi.php
+++ oop_2/Taxi.php
@@ -1,33 +1,32 @@
 <?php
 
+require_once __DIR__ . '/Car.php';
+
 class Taxi extends Car
 {
-    private $passenger;
-    private $pickUp_number;
-    private $lower_number;
+    private $passenger = 0;
 
     public function __construct($name, $number, $color)
     {
         parent::__construct($name, $number, $color);
-        $this->passenger = 0;
     }
 
     public function pickUp($pickUp_number)
     {
+        $this->passenger += $pickUp_number; 
         $pick_num = strval($pickUp_number);
-        $this->passenger += $pick_num; 
-        echo $pick_num . "人乗車しました。\n";
+        return $pick_num . "人乗車しました。\n";
         
     }
 
     public function lower($lower_number)
     {
+        $this->passenger -= $lower_number;
         $lower_num = strval($lower_number);
-        $this->passenger -= $lower_num;
         if ($this->passenger >= 0) {
-            echo $lower_num . "人降車しました。\n";
+            return $lower_num . "人降車しました。\n";
         } else {
-            echo $lower_num . "人は降車できません。\n";
+            return $lower_num . "人は降車できません。\n";
         }
     }
 
